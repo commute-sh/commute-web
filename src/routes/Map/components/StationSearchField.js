15cdbe40142783b05ca6ead692265798aa4c679c
Alexis Kinsella <alexis.kinsella@gmail.com>
2016-11-14 23:36:44 +0100

    Replaced AutoComplete with TextField

--- src/routes/Map/components/StationSearchField.js
+++ src/routes/Map/components/StationSearchField.js
@@ -1,7 +1,7 @@
 import React, { PropTypes } from 'react';
 
 import Paper from 'material-ui/Paper';
-import AutoComplete from 'material-ui/AutoComplete';
+import TextField from 'material-ui/TextField';
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -10,30 +10,23 @@ import AutoComplete from 'material-ui/AutoComplete';
 
 class StationSearchField extends React.Component {
 
-  constructor(props) {
-    super(props);
+  onChange() {
 
-    this.state = {
-      dataSource: [],
-    };
   }
-
-  handleUpdateInput = (value) => {
-
-  };
 
   render() {
 
     return (
      <Paper zDepth={0} style={{ margin: '0 20px 20px 20px' }}>
-        <AutoComplete
-          hintText="Nom de station"
-          dataSource={this.state.dataSource}
-          onUpdateInput={this.handleUpdateInput}
-          floatingLabelText="Recherche"
-          fullWidth={true}
-          textFieldStyle={{ width: '100%' }}
-        />
+
+       <TextField
+         hintText="Nom de station"
+         floatingLabelText="Recherche"
+         fullWidth={true}
+         style={{ width: '100%' }}
+         onChange={this.onChange.bind(this)}
+       />
+
       </Paper>
     );
 
