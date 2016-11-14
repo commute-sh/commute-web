36d4e378de6f3ecf36e2bc715359304a7dfc78c2
Alexis Kinsella <alexis.kinsella@gmail.com>
2016-11-14 23:30:02 +0100

    CMT-21 - Graphql fail to answer with correct response on first call. Simplified query of stations in graphql server

--- server/graphql/queries/station.js
+++ server/graphql/queries/station.js
@@ -4,43 +4,10 @@ const List = graphql.GraphQLList
 const StationType = require('../types/StationType')
 require('whatwg-fetch')
 
-// React.js News Feed (RSS)
-const url = 'http://api.commute.sh/stations?contract-name=Paris';
-
-let items = [];
-let lastFetchTask;
-let lastFetchTime = new Date(1970, 0, 1);
-
-const stations = {
+module.exports = {
   type: new List(StationType),
   resolve() {
-    if (lastFetchTask) {
-      return lastFetchTask;
-    }
-
-    if ((new Date() - lastFetchTime) > 1000 * 60 * 10 /* 10 mins */) {
-      lastFetchTime = new Date();
-      lastFetchTask = fetch(url)
-        .then(response => response.json())
-        .then(data => {
-
-          items = data;
-
-          return items;
-        })
-        .finally(() => {
-          lastFetchTask = null;
-        });
-
-      if (items.length) {
-        return items;
-      }
-
-      return lastFetchTask;
-    }
-
-    return items;
-  },
+    return fetch('http://api.commute.sh/stations?contract-name=Paris')
+      .then(response => response.json())
+  }
 };
-
-module.exports = stations;
