1b93d9bb3c7b5711dc7103e721e3a646a2f73d8d
Alexis Kinsella <alexis.kinsella@gmail.com>
2017-01-02 00:24:52 +0100

    Using now 128px width thumbs

--- src/routes/Map/components/StationItem.js
+++ src/routes/Map/components/StationItem.js
@@ -19,11 +19,11 @@ class StationItem extends React.Component {
 
   render() {
     const { station } = this.props;
-    const { images } = station;
+    const images = (station.images || []).filter(image => image.width === 128);
 
-    const avatarUrl = images && images.length > 0 ?
+    const avatarUrl = images.length > 0 ?
       `https://s3-eu-west-1.amazonaws.com/image-commute-sh/contracts/${station.contract_name}/${station.contract_name}-${station.number}-${images[0].uid}-${images[0].width}-${images[0].quality}.jpg` :
-      `https://s3-eu-west-1.amazonaws.com/image-commute-sh/contracts/${station.contract_name}/${station.contract_name}-${1}-${640}-${60}.jpg`;
+      `https://s3-eu-west-1.amazonaws.com/image-commute-sh/contracts/${station.contract_name}/${station.contract_name}-${1}-${128}-${100}.jpg`;
 
     return (
       <ListItem
